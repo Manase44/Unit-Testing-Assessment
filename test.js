83b95969da87e6af5372bb90ac8a5bdb5a83af0c
Manase44 <fikopersempre@gail.com>
2024-05-28 14:31:43 +0300

    structure the calculateGradeAndAverage function

---
+++ test.js
@@ -0,0 +1,19 @@
+const test = (object) => {
+    Name = object.name;
+    Mathematics = object.math;
+    English = object.eng;
+    Kiswahili = object.kis;
+    Physics = object.phy;
+    Chemistry = object.che;
+    ReligiousEducation = object.re;
+    ComputerStudies = object.comp;
+
+    
+
+    console.log(Mathematics, English)
+}
+
+
+test({name: "Dennis Doe", math: 80, eng: 65, kis: 48, phy: 67, che: 56, re:89, comp: 82})
+
+  
